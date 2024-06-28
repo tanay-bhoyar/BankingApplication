0fd801dd6b8a6111c8a91722ae8a5f9cd61634c2
tanay-bhoyar <66437798+tanay-bhoyar@users.noreply.github.com>
2024-06-28 21:04:51 +0530

    implement user title

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalance from "@/components/TotalBalance";
 import React from "react";
 
 const Home=()=>{
+
+    const loggedIn = {firstName:"Tanay"};
+
     return(
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently"
+                    />
+
+                    <TotalBalance
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.37}
+                    />
+
+                </header>
+            </div>
+        </section>
     )
 }
 
