fb7c94274208c2db679856b29798b910f73915e0
tanay-bhoyar <66437798+tanay-bhoyar@users.noreply.github.com>
2024-07-11 14:55:34 +0530

    implement replay system for monitoring using sentry

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,10 +11,10 @@ export default function RootLayout({
           <div className="auth-asset">
             <div>
               <Image 
-                src="/icons/auth-image.svg"
+                src="/icons/blue_horizon_4k.jpg"
                 alt="Auth image"
-                width={500}
-                height={500}
+                width={800}
+                height={1000}
               />
             </div>
           </div>
