10555ec8938e01afa701506035eec6681a3d166f
tanay-bhoyar <66437798+tanay-bhoyar@users.noreply.github.com>
2024-07-12 21:52:29 +0530

    finalize the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,22 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalance from "@/components/TotalBalance";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home= async()=>{
+const Home= async({searchParams:{id,page}}:SearchParamProps)=>{
+
+    const currentPage = Number(page as string || 1);
 
     const loggedIn = await getLoggedInUser();
+    const accounts=await getAccounts({userId:loggedIn.$id})
+    if(!accounts) return;
+    const accountsData=accounts?.data;
+    const appwriteItemId=(id as string) || accountsData[0]?.appwriteItemId;
+    const account=await getAccount({appwriteItemId})
+
 
     return(
         <section className="home">
@@ -15,24 +25,29 @@ const Home= async()=>{
                     <HeaderBox 
                         type="greeting"
                         title="Welcome"
-                        user={loggedIn?.name || 'Guest'}
+                        user={loggedIn?.firstName || 'Guest'}
                         subtext="Access and manage your account and transactions efficiently"
                     />
 
                     <TotalBalance
-                        accounts={[]}
-                        totalBanks={1}
-                        totalCurrentBalance={1250.37}
+                        accounts={accountsData}
+                        totalBanks={accounts?.totalBanks}
+                        totalCurrentBalance={accounts?.totalCurrentBalance}
                     />
 
                 </header>
-                RECENT TRANSACTIONS
+                <RecentTransactions 
+                    accounts={accountsData}
+                    transactions={account?.transactions}
+                    appwriteItemId={appwriteItemId}
+                    page={currentPage}
+                />
             </div>
 
             <RightSidebar 
                 user = {loggedIn}
-                transactions={[]}
-                banks ={[{currentBalance:123.50},{currentBalance:500.50}]}
+                transactions={account?.transactions}
+                banks ={accountsData?.slice(0,2)}
             />
         </section>
     )
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,11 +2,12 @@ import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Copy from "./Copy";
 
 const BankCard = ({account , userName , showBalance=true}:CreditCardProps)=>{
     return(
         <div className="flex flex-col">
-            <Link href ="/" className="bank-card">
+            <Link href ={`/transaction-history/?id=${account.appwriteItemId}`} className="bank-card">
                 <div className="bank-card_content">
                     <div>
                         <h1 className="text-16 font-semibold text-white">{userName}</h1>
@@ -17,14 +18,14 @@ const BankCard = ({account , userName , showBalance=true}:CreditCardProps)=>{
                     <article className="flex flex-col gap-2">
                         <div className="flex justify-between">
                             <h1 className="text-12 font-semibold text-white">
-                                {userName}
+                                {account.name}
                             </h1>
                             <h2 className="text-12 font-semibold text-white">
                             ◍◍ / ◍◍
                             </h2>
                         </div>
                         <p className="text-14 font-semibold tracking-[1.1px] text-white">
-                            ◍◍◍◍ ◍◍◍◍ ◍◍◍◍ <span className="text-16">{1234}</span>
+                            ◍◍◍◍ ◍◍◍◍ ◍◍◍◍ <span className="text-16">{account?.mask}</span>
                         </p>
                     </article>
                 </div>
@@ -50,7 +51,7 @@ const BankCard = ({account , userName , showBalance=true}:CreditCardProps)=>{
                 className="absolute top-0 left-0"
                 />
             </Link>
-            {/* COPY */}
+            {showBalance && <Copy title={account?.shareableId} />}
         </div>
     )
 }
