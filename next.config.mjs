8f1f2d75841ec49ceb4461e2350a557c366b00fc
tanay-bhoyar <66437798+tanay-bhoyar@users.noreply.github.com>
2024-07-13 11:17:05 +0530

    new try

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript:{
-        ignoreBuildErrors:true
-    },
-    eslint:{
-        ignoreDuringBuilds:true
-    }
+    
 };
 
 export default withSentryConfig(nextConfig, {
